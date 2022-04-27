6472e2f72ead775abc56d6d900f9909a5d142b92
Carzate <carzate@applab.mx>
2022-04-27 14:42:45 -0500

    feat: agregar icono

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="py-10">
-        <h3 class="font-bold text-4xl lg:pl-8 dark:text-white">Mis búsquedas</h3>
+        <div class="flex">
+            <h3 class="font-bold text-4xl lg:pl-8 dark:text-white">Mis búsquedas</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+        </div>
         <p class="text-sm pl-8 pt-4 dark:text-white">Últimas 10 búsquedas, puedes hacer click en cada una para volver a buscar</p>
         <div class="pt-8 pl-8" v-if="record.length > 0">
             <ul class="w-1/2 text-sm font-medium border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
